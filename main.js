9cb1696de1f1b0f809f3b6a006ece76ba1221e8d
Adrian_Peña <adrianchooo18@gmail.com>
2021-11-14 15:45:33 -0500

    toggle finished

--- main.js
+++ main.js
@@ -17,10 +17,31 @@ closeMenu.addEventListener('click', () => {
 });
 
 //Dissapear menu when I click
-const navLink = document.querySelector('.nav__link');
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
   navMenu.classList.remove('show')
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+//Behavior active link while scroll actions
+const sections = document.querySelectorAll('section[id]');
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active');
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active');
+    }
+  })
+}
